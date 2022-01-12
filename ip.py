cb3045a0db870f46d657e9c43d48c849bd354601
Al-khalid <92049116+Al-khalid@users.noreply.github.com>
2022-01-12 15:45:49 +0300

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,4 +1,27 @@
+## libraries
 import requests
-res=requests.get("https://ipinfo.io/")
-data=res.json()
+import colorama
+from colorama import Fore, Back, Style
+import pyfiglet                                                                                                                                                
+                                                                                                    
+
+print(
+    '''
+ ____        
+(҂`_´)
+<,︻╦╤─ ҉ - -
+_/﹋\_
+   '''
+)
+
+
+pii = pyfiglet.figlet_format("IP Information")
+print(pii)
+
+
+ip = input(f"{Fore.CYAN} - Enter Target IP : ")
+
+res = requests.get("https://ipinfo.io/")
+data = res.json()
 print(data)
+print(Style.RESET_ALL)
